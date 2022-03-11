a8ef1e8f664e5e60816f23e415c3c2e80bd75fb9
aletna <dennis.antela@gmail.com>
2022-03-11 14:11:39 -0500

    comment out wallet setup for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,34 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
-import dynamic from "next/dynamic";
-import { ConnectionProvider } from "@solana/wallet-adapter-react";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import { useMemo } from "react";
+// import dynamic from "next/dynamic";
+// import { ConnectionProvider } from "@solana/wallet-adapter-react";
+// import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+// import { useMemo } from "react";
 
-const SOLANA_NETWORK = WalletAdapterNetwork.Devnet;
-const network = SOLANA_NETWORK;
-let _endpoint = "https://api.devnet.solana.com";
-if (network !== WalletAdapterNetwork.Devnet) {
-  _endpoint = "https://api.mainnet-beta.solana.com";
-}
+// const SOLANA_NETWORK = WalletAdapterNetwork.Devnet;
+// const network = SOLANA_NETWORK;
+// let _endpoint = "https://api.devnet.solana.com";
+// if (network !== WalletAdapterNetwork.Devnet) {
+//   _endpoint = "https://api.mainnet-beta.solana.com";
+// }
 
-const WalletProvider = dynamic(
-  () => import("../contexts/ClientWalletProvider"),
-  {
-    ssr: false,
-  }
-);
+// const WalletProvider = dynamic(
+//   () => import("../contexts/ClientWalletProvider"),
+//   {
+//     ssr: false,
+//   }
+// );
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const endpoint = useMemo(() => _endpoint, []);
+  // const endpoint = useMemo(() => _endpoint, []);
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider>
-        <Component {...pageProps} />;
-      </WalletProvider>
-    </ConnectionProvider>
+    <>
+      {/* <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider> */}
+      <Component {...pageProps} />;
+      {/* </WalletProvider>
+      </ConnectionProvider> */}
+    </>
   );
 }
 
